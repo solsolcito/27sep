efce028ddbd41a486282126dca972903ccae9a7b
solange alarcon <solealarconmelinao@gmail.com>
2022-09-27 14:41:24 -0300

    clase del 27sep ciclos

---
+++ suma.js
@@ -0,0 +1,18 @@
+const numbers = [18,20,19,26,25,28]
+
+let index = 0
+let sum = 0
+do {
+    sum = sum + numbers[index]// sum += numbers[index]
+    index++
+    console.log(sum)
+
+
+}while(index < numbers.length)
+console.log(sum)
+
+
+
+
+
+
